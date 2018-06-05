f8ed95579dd68373fc71a6f5f8addfaefd435a1c
bgulbis <brian.gulbis@gmail.com>
2018-06-05 12:52:11 -0500

    compare orders with admin data

--- src/2018-05-31_cathflo.R
+++ src/2018-05-31_cathflo.R
@@ -30,4 +30,52 @@ meds <- read_data(dir_raw, "meds-inpt", FALSE) %>%
     ) %>%
     mutate_at("med.dose", factor)
 
+# find missing -----------------------------------------
 
+test <- read_data(dir_raw, "test", FALSE) %>%
+    as.patients()
+
+charges <- "data/external/2018-05_cathflo/charges_2017-04_2017-06.csv" %>%
+    read_csv() %>%
+    # filter(
+    #     qty < 10,
+    #     qty > 0
+    # ) %>%
+    mutate_at(
+        "acct",
+        str_replace_all,
+        pattern = "^C0",
+        replacement = ""
+    )
+
+test_fin <- concat_encounters(charges$acct)
+
+# run MBO query
+#   * Identifiers - by FIN
+
+test_id <- read_data(dir_raw, "id-fin", FALSE) %>%
+    as.id()
+
+x <- semi_join(test, test_id, by = "millennium.id")
+y <- anti_join(test_id, test, by = "millennium.id") %>%
+    left_join(charges, by = c("fin" = "acct"))
+
+pts_orders <- read_data(dir_raw, "patients-orders", FALSE) %>%
+    as.patients()
+
+mbo_orders <- concat_encounters(pts_orders$millennium.id)
+
+# run MBO query
+#   * Orders Meds - Details with Volume
+
+cathflo <- "alteplase 1 mg INJ (2 mg/2 ml)"
+
+orders <- read_data(dir_raw, "orders-details", FALSE) %>%
+    as.order_detail(
+        extras = c(
+            "order.product" = "Mnemonic (Product)",
+            "order.as" = "Mnemonic (Ordered As Name)"
+        )
+    ) %>%
+    mutate_at("ingredient.dose", as.numeric) %>%
+    filter(order.product == cathflo | order.as == cathflo)
