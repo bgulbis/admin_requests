298a872237c79e4275e9913a77ddbacdb10a5f1d
Brian Gulbis <brian.gulbis@gmail.com>
2018-07-16 13:55:53 -0500

    make data set

--- src/2018-07-16_med-requests.R
+++ src/2018-07-16_med-requests.R
@@ -1,3 +1,11 @@
 library(tidyverse)
 library(lubridate)
 library(readxl)
+
+file_nm <- "data/raw/2018-07_med-requests/2018-06-01_2018-07-11_med-requests.xlsx"
+data_reqs <- file_nm %>%
+    read_excel() %>%
+    rename_all(str_to_lower)
+    
+data_reqs %>%
+    write_rds(path = "data/tidy/2018-07_med-requests/med-requests.Rds")
