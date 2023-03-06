bfd3f98ee810b2b26f63172b2217fca857187a8b
bgulbis <brian.gulbis@gmail.com>
2023-03-06 14:21:17 -0600

    count doses dispensed

---
+++ src/dispense_doses_count.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(readxl)
+library(mbohelpr)
+library(openxlsx)
+
+f <- set_data_path("admin_requests", "")
+
+df_count <- read_excel(paste0(f, "raw/dispense_doses_count.xlsx")) |> 
+    rename_all(str_to_lower) |> 
+    mutate(
+        pyxis = disp_event_type == "Device Dispense",
+        across(pyxis, ~coalesce(., FALSE))
+    ) |> 
+    group_by(facility, pyxis) |> 
+    summarize(
+        across(num_doses_dispensed, \(x) sum(x, na.rm = TRUE)),
+        .groups = "drop"
+    ) |> 
+    mutate(across(pyxis, \(x) if_else(x, "Pyxis", "Pharmacy"))) |> 
+    pivot_wider(names_from = pyxis, values_from = num_doses_dispensed)
+
+write.xlsx(df_count, paste0(f, "final/dispense_doses_count.xlsx"), overwrite = TRUE)
